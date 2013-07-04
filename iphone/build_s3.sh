bc2f951035e9058a2ed4a05b94038d80cb3569b3
Jan-Rixt Van Hoye <jixt@burntide.eu>
2013-07-04 11:06:17 +0200

    all S3 put commands now have the config file parameter option.

--- iphone/build_s3.sh
+++ iphone/build_s3.sh
@@ -195,15 +195,15 @@ for SDK in $SDKS; do
 		mv "${OUTPUT}/${OTA_NAME}" "${OUTPUT}/${LCASE_OTA_NAME}"
 		
 		if [ "$S3_CMD_CONFIG_FILE" == "" ]; then
-			$S3_CMD put -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
-			$S3_CMD put -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
+			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
+			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
 			if [ "$OTASmallIcon" != "" ]
 			then
-				$S3_CMD put "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
+				$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
 			fi
 		else
-			$S3_CMD put -c "~/.$S3_CMD_CONFIG_FILE" -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
-			$S3_CMD put -c "~/.$S3_CMD_CONFIG_FILE" -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
+			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
+			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
 			if [ "$OTASmallIcon" != "" ]
 			then
 				$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE  "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
