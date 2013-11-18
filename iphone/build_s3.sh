fd18f87083ea92f2cc2e3d33983fcfef778f707e
Jan-Rixt Van Hoye <jixt@burntide.eu>
2013-11-18 17:07:37 +0100

    now possible to choose xcode project file when there are multiple.

--- iphone/build_s3.sh
+++ iphone/build_s3.sh
@@ -6,15 +6,13 @@
 #
 
 #!/bin/sh
-function failed()
-{
+function failed() {
     echo "Failed: $@" >&2
     exit 1
 }
 
-function usage()
-{
-    echo "Usage: $0 -s3b s3bucket (-c configfile -s3c s3configfile)"
+function usage() {
+    echo "Usage: $0 -xc xcode_config -s3b s3bucket (-c configfile -s3c s3configfile)"
     exit 2
 }
 
@@ -28,12 +26,13 @@ do
         -c) BUILD_CONFIG_FILE=$2; shift;;
 		-s3c) S3_CMD_CONFIG_FILE=$2; shift;;
 		-s3b) S3_BUCKET=$2; shift;;
+		-xc) XCODE_CONFIG=$2; shift;;
         *)	usage;;
     esac
     shift
 done
 
-if [ "$S3_BUCKET" == ""]
+if [ "$S3_BUCKET" == "" -o "$XCODE_CONFIG" == ""]
 then
 	usage;
 fi
@@ -85,49 +84,49 @@ fi
 # Build the application for the several levels (Debug, Release, ...) &
 # create an ipa out of them
 
-for SDK in $SDKS; do
-    for CONFIG in $CONFIGURATIONS; do
-        # Set the bundle identifier
-        $PLIST_BUDDY -c "Set :CFBundleIdentifier $(eval echo \$`echo BundleIdentifier$CONFIG`)" "$INFO_PLIST"
-        # Set variables
-		PROVISIONING=$(eval echo \$`echo Provision$CONFIG`)
-        CERTIFICATE="$PROVISIONING_PROFILE_PATH/$PROVISIONING.mobileprovision"
-		# Build
-        if [ "$TARGET_NAME" != "" ] 
-        then
-            $XCODEBUILD -configuration $CONFIG -target "$TARGET_NAME" -sdk $SDK clean;
-            $XCODEBUILD -configuration $CONFIG -target "$TARGET_NAME" -sdk $SDK || failed build;
-        else
-            $XCODEBUILD -configuration $CONFIG -sdk $SDK clean;
-            $XCODEBUILD -configuration $CONFIG -sdk $SDK || failed build;
-        fi
-		# Create ipa
-		OTA_NAME="$APP_FILENAME-$CONFIG-manifest.plist"
-		IPA_NAME="$APP_FILENAME-$CONFIG.ipa"
-		OTA_URL="$(eval echo \$`echo OTAUrl$CONFIG`)"
-		APP_FILE=`find "$WORKSPACE/build/$CONFIG-iphoneos" -name "*.app"`
-        $XCRUN -sdk $SDK PackageApplication -v "$APP_FILE" -o "$OUTPUT/$IPA_NAME" --sign "$(eval echo \$`echo Codesign$CONFIG`)" --embed "$CERTIFICATE";
-        # Zip & Copy the dSYM file & remove the zip
-        cd "$WORKSPACE/build/$CONFIG-iphoneos/"
-        tar -pczf "$APP_FILENAME.tar.gz" "$APP_FILENAME.app.dSYM"
-        cd "$WORKSPACE"
-        cp "$WORKSPACE/build/$CONFIG-iphoneos/$APP_FILENAME.tar.gz" "$OUTPUT/$APP_FILENAME.tar.gz"
-        rm "$WORKSPACE/build/$CONFIG-iphoneos/$APP_FILENAME.tar.gz"
-        # Copy the icon files
-		if [ -f "$WORKSPACE/$OTASmallIcon" ]; then
-			cp "$WORKSPACE/$OTASmallIcon" "$OUTPUT/Icon-57.png"
-		fi
-		if [ -f "$WORKSPACE/$OTALargeIcon" ]; then
-			cp "$WORKSPACE/$OTALargeIcon" "$OUTPUT/Icon-512.png"
-		fi
-        # Copy the release noteS
-        if [ -f "$WORKSPACE/$RELEASENOTE" ]; then
-            cp "$WORKSPACE/$RELEASENOTE" "$OUTPUT/$RELEASENOTE"
-        fi
-		# Create the manifest file
-		bundle_version=$(defaults read "$WORKSPACE/$INFO_PLIST" CFBundleShortVersionString)
-		bundle_id=$(defaults read "$WORKSPACE/$INFO_PLIST" CFBundleIdentifier)
-		cat <<-EOF > $OUTPUT/$OTA_NAME
+SDK="iphoneos"
+
+# Set the bundle identifier
+$PLIST_BUDDY -c "Set :CFBundleIdentifier $(eval echo \$`echo BundleIdentifier$XCODE_CONFIG`)" "$INFO_PLIST"
+# Set variables
+PROVISIONING=$(eval echo \$`echo Provision$XCODE_CONFIG`)
+CERTIFICATE="$PROVISIONING_PROFILE_PATH/$PROVISIONING.mobileprovision"
+# Build
+if [ "$TARGET_NAME" != "" ] 
+then
+	$XCODEBUILD -configuration $XCODE_CONFIG -target "$TARGET_NAME" -sdk $SDK clean;
+    $XCODEBUILD -configuration $XCODE_CONFIG -target "$TARGET_NAME" -sdk $SDK || failed build;
+else
+    $XCODEBUILD -configuration $XCODE_CONFIG -sdk $SDK clean;
+    $XCODEBUILD -configuration $XCODE_CONFIG -sdk $SDK || failed build;
+fi
+# Create ipa
+OTA_NAME="$APP_FILENAME-$XCODE_CONFIG-manifest.plist"
+IPA_NAME="$APP_FILENAME-$XCODE_CONFIG.ipa"
+OTA_URL="$(eval echo \$`echo OTAUrl$XCODE_CONFIG`)"
+APP_FILE=`find "$WORKSPACE/build/$XCODE_CONFIG-iphoneos" -name "*.app"`
+$XCRUN -sdk $SDK PackageApplication -v "$APP_FILE" -o "$OUTPUT/$IPA_NAME" --sign "$(eval echo \$`echo Codesign$XCODE_CONFIG`)" --embed "$CERTIFICATE";
+# Zip & Copy the dSYM file & remove the zip
+cd "$WORKSPACE/build/$XCODE_CONFIG-iphoneos/"
+tar -pczf "$APP_FILENAME.tar.gz" "$APP_FILENAME.app.dSYM"
+cd "$WORKSPACE"
+cp "$WORKSPACE/build/$XCODE_CONFIG-iphoneos/$APP_FILENAME.tar.gz" "$OUTPUT/$APP_FILENAME.tar.gz"
+rm "$WORKSPACE/build/$XCODE_CONFIG-iphoneos/$APP_FILENAME.tar.gz"
+# Copy the icon files
+if [ -f "$WORKSPACE/$OTASmallIcon" ]; then
+	cp "$WORKSPACE/$OTASmallIcon" "$OUTPUT/Icon-57.png"
+fi
+if [ -f "$WORKSPACE/$OTALargeIcon" ]; then
+	cp "$WORKSPACE/$OTALargeIcon" "$OUTPUT/Icon-512.png"
+fi
+# Copy the release noteS
+if [ -f "$WORKSPACE/$RELEASENOTE" ]; then
+	cp "$WORKSPACE/$RELEASENOTE" "$OUTPUT/$RELEASENOTE"
+fi
+# Create the manifest file
+bundle_version=$(defaults read "$WORKSPACE/$INFO_PLIST" CFBundleShortVersionString)
+bundle_id=$(defaults read "$WORKSPACE/$INFO_PLIST" CFBundleIdentifier)
+cat <<-EOF > $OUTPUT/$OTA_NAME
 		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 		<plist version="1.0">
 		<dict>
@@ -143,7 +142,7 @@ for SDK in $SDKS; do
 		                   <string>{{ipa}}</string>
 		               </dict>
 		EOF
-		if [ -f "$WORKSPACE/$OTASmallIcon" ]; then
+if [ -f "$WORKSPACE/$OTASmallIcon" ]; then
 			cat <<-EOF >> $OUTPUT/$OTA_NAME
 		               <dict>
 		                   <key>kind</key>
@@ -154,8 +153,8 @@ for SDK in $SDKS; do
 		                   <string>{{icon57}}</string>
 		               </dict>
 		EOF
-		fi
-		if [ -f "$WORKSPACE/$OTALargeIcon" ]; then
+fi
+if [ -f "$WORKSPACE/$OTALargeIcon" ]; then
 			cat <<-EOF >> $OUTPUT/$OTA_NAME
 		               <dict>
 		                   <key>kind</key>
@@ -166,7 +165,7 @@ for SDK in $SDKS; do
 		                   <string>{{icon512}}</string>
 		               </dict>
 		EOF
-		fi
+fi
 		cat <<-EOF >> $OUTPUT/$OTA_NAME
 		           </array>
 		           <key>metadata</key>
@@ -186,31 +185,28 @@ for SDK in $SDKS; do
 		</plist>
 		EOF
 		
-		# Upload files to Amazon S3
+# Upload files to Amazon S3
 		
-		LCASE_IPA_NAME=`lowerCase "$IPA_NAME"`
-		LCASE_OTA_NAME=`lowerCase "$OTA_NAME"`
+LCASE_IPA_NAME=`lowerCase "$IPA_NAME"`
+LCASE_OTA_NAME=`lowerCase "$OTA_NAME"`
 		
-		mv "${OUTPUT}/${IPA_NAME}" "${OUTPUT}/${LCASE_IPA_NAME}"
-		mv "${OUTPUT}/${OTA_NAME}" "${OUTPUT}/${LCASE_OTA_NAME}"
+mv "${OUTPUT}/${IPA_NAME}" "${OUTPUT}/${LCASE_IPA_NAME}"
+mv "${OUTPUT}/${OTA_NAME}" "${OUTPUT}/${LCASE_OTA_NAME}"
 		
-		if [ "$S3_CMD_CONFIG_FILE" == "" ]; then
-			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
-			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
-			if [ "$OTASmallIcon" != "" ]
-			then
-				$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
-			fi
-		else
-			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
-			$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
-			if [ "$OTASmallIcon" != "" ]
-			then
-				$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE  "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
-			fi
-		fi
+if [ "$S3_CMD_CONFIG_FILE" == "" ]; then
+	$S3_CMD put -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
+	$S3_CMD put -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
+	if [ "$OTASmallIcon" != "" ]
+	then
+		$S3_CMD put "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
+	fi
+else
+	$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "application/octet-stream" "$OUTPUT/$LCASE_IPA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_IPA_NAME"
+	$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE -m "text/xml" "$OUTPUT/$LCASE_OTA_NAME" "$S3_UPLOAD_LOCATION/$LCASE_OTA_NAME"
+	if [ "$OTASmallIcon" != "" ]
+	then
+		$S3_CMD put -c ~/.$S3_CMD_CONFIG_FILE  "$WORKSPACE/$OTASmallIcon" "$S3_UPLOAD_LOCATION/Icon-57.png"
+	fi
+fi
 		
-    done
-done
-
-
+# And now you're done!
