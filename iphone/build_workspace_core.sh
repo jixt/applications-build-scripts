4f8ca3f1f7d00035bbcb5cc511aef194a383351c
Jan-Rixt Van Hoye <jixt@burntide.eu>
2014-05-11 16:46:44 +0200

    - removed the pipestatus from the clean build for real.

--- iphone/build_workspace_core.sh
+++ iphone/build_workspace_core.sh
@@ -13,10 +13,10 @@ source ./build_preconfig.sh
 # Now build the application
 echo "Build the application"
 
-$XCODEBUILD -configuration "$XCODE_CONFIG" -sdk $SDK clean | xcpretty -c && exit ${PIPESTATUS[0]};
+$XCODEBUILD -configuration "$XCODE_CONFIG" -sdk $SDK clean | xcpretty -c;
 if [ "$APP_NAME" != "" ] 
 then
-	$XCODEBUILD -workspace "$WORKSPACE_NAME".xcworkspace -scheme "$SCHEME_NAME" -configuration "$XCODE_CONFIG" -sdk $SDK APP_NAME=$APP_NAME BUNDLE_ID=$BUNDLE_IDENTIFIER build PROVISIONING_PROFILE="$PROVISIONING" CODE_SIGN_IDENTITY="iPhone Distribution: $CODE_SIGN_IDENTITY" CONFIGURATION_BUILD_DIR="$WORKSPACE/build/$XCODE_CONFIG-iphoneos" || failed build | xcpretty -c;
+	$XCODEBUILD -workspace "$WORKSPACE_NAME".xcworkspace -scheme "$SCHEME_NAME" -configuration "$XCODE_CONFIG" -sdk $SDK APP_NAME=$APP_NAME BUNDLE_ID=$BUNDLE_IDENTIFIER build PROVISIONING_PROFILE="$PROVISIONING" CODE_SIGN_IDENTITY="iPhone Distribution: $CODE_SIGN_IDENTITY" CONFIGURATION_BUILD_DIR="$WORKSPACE/build/$XCODE_CONFIG-iphoneos" || failed build | xcpretty -c && exit ${PIPESTATUS[0]};
 else
 	$XCODEBUILD -workspace "$WORKSPACE_NAME".xcworkspace -scheme "$SCHEME_NAME" -configuration "$XCODE_CONFIG" -sdk $SDK BUNDLE_ID=$BUNDLE_IDENTIFIER build PROVISIONING_PROFILE="$PROVISIONING" CODE_SIGN_IDENTITY="iPhone Distribution: $CODE_SIGN_IDENTITY" CONFIGURATION_BUILD_DIR="$WORKSPACE/build/$XCODE_CONFIG-iphoneos" || failed build | xcpretty -c && exit ${PIPESTATUS[0]};
 fi
