dea7b93ec66b45e95737c0c0d7cbd0cc3717cf86
Jan-Rixt Van Hoye <jixt@burntide.eu>
2014-09-17 21:19:47 +0200

    - Fix for dSYM upload problem

--- iphone/build_koomoda_v2.sh
+++ iphone/build_koomoda_v2.sh
@@ -82,7 +82,7 @@ LCASE_OTA_NAME=`lowerCase "$OTA_NAME"`
 mv "${OUTPUT}/${IPA_NAME}" "${OUTPUT}/${LCASE_IPA_NAME}"
 mv "${OUTPUT}/${DSYM_NAME}" "${OUTPUT}/${LCASE_DSYM_NAME}"
 mv "${OUTPUT}/${OTA_NAME}" "${OUTPUT}/${LCASE_OTA_NAME}"
-curl -3 $KOOMODA_API_URL -F file=@"${OUTPUT}/${LCASE_DSYM_NAME}" -F file=@"${OUTPUT}/${LCASE_IPA_NAME}" -F icon=@"${OUTPUT}/Icon-57.png" -F manifest=@"${OUTPUT}/${LCASE_OTA_NAME}" -F user_token="${K_ACCOUNT_TOKEN}" -F app_token="${K_APP_TOKEN}" -F app_version="${BUILD_NUMBER}"
+curl -3 $KOOMODA_API_URL -F dsym=@"${OUTPUT}/${LCASE_DSYM_NAME}" -F file=@"${OUTPUT}/${LCASE_IPA_NAME}" -F icon=@"${OUTPUT}/Icon-57.png" -F manifest=@"${OUTPUT}/${LCASE_OTA_NAME}" -F user_token="${K_ACCOUNT_TOKEN}" -F app_token="${K_APP_TOKEN}" -F app_version="${BUILD_NUMBER}"
 
 
 # And now you're done!
